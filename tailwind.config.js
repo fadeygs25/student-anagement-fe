a43b65a949e260f10f522db3fe1ae907e72be9a4
Dung Nguyen <nqdd251003@gmail.com>
2024-09-17 19:58:36 +0700

    [Feature] Add framework

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './components/**/*.{vue,js}',
+      './layouts/**/*.vue',
+      './pages/**/*.vue',
+      './plugins/**/*.{js,ts}',
+      './nuxt.config.{js,ts}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
